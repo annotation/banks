3581c0ad76ee6ed61d543948ca888f685698cb75
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2019-04-09 16:15:00 +0200

    prepared for sims

--- tf/0.2/oslots.tf
+++ tf/0.2/oslots.tf
@@ -3,11 +3,12 @@
 @name=Culture quotes from Iain Banks
 @purpose=exposition
 @source=Good Reads
+@status=with for similarities in a separate module
 @url=https://www.goodreads.com/work/quotes/14366-consider-phlebas
 @valueType=str
 @version=0.2
 @writtenBy=Text-Fabric
-@dateWritten=2019-04-09T14:11:20Z
+@dateWritten=2019-04-09T14:14:14Z
 
 100	1-99
 1-55
--- tf/0.2/terminator.tf
+++ tf/0.2/terminator.tf
@@ -4,11 +4,12 @@
 @name=Culture quotes from Iain Banks
 @purpose=exposition
 @source=Good Reads
+@status=with for similarities in a separate module
 @url=https://www.goodreads.com/work/quotes/14366-consider-phlebas
 @valueType=str
 @version=0.2
 @writtenBy=Text-Fabric
-@dateWritten=2019-04-09T14:11:20Z
+@dateWritten=2019-04-09T14:14:14Z
 
 103	,
 ,
