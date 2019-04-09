33f757ed47d14816f523162244c2e31dcaf0a208
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2019-04-09 16:15:00 +0200

    prepared for sims

--- tf/0.2/otext.tf
+++ tf/0.2/otext.tf
@@ -6,8 +6,9 @@
 @sectionFeatures=title,number,number
 @sectionTypes=book,chapter,sentence
 @source=Good Reads
+@status=with for similarities in a separate module
 @url=https://www.goodreads.com/work/quotes/14366-consider-phlebas
 @version=0.2
 @writtenBy=Text-Fabric
-@dateWritten=2019-04-09T14:11:20Z
+@dateWritten=2019-04-09T14:14:14Z
 
