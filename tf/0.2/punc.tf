436e0d99b023983b9a8f52d01f7bce64fd985871
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2019-04-09 16:12:00 +0200

    version 0.2

---
+++ tf/0.2/punc.tf
@@ -0,0 +1,29 @@
+@node
+@compiler=Dirk Roorda
+@description=the punctuation after a word
+@name=Culture quotes from Iain Banks
+@purpose=exposition
+@source=Good Reads
+@url=https://www.goodreads.com/work/quotes/14366-consider-phlebas
+@valueType=str
+@version=0.2
+@writtenBy=Text-Fabric
+@dateWritten=2019-04-09T14:11:20Z
+
+3	,
+6	,
+20	;
+24	,
+27	.
+38	,
+45	,
+51	,
+55	?
+,
+75	,
+78	,
+,
+,
+83	,
+88	,
+99	.
